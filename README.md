ee594232e5042cd822b457a4d1f2fa4cb90e8c0f
tosikko <ajwadjaved@gmail.com>
2019-09-14 15:29:58 +0000

    added comments, changed file naming convention

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Scraps data off from www.annualreports.com
+Scraps data off from www.annualreports.com, around 71,000 in total right now.
--- scrap_pdfs.py
+++ scrap_pdfs.py
@@ -13,23 +13,27 @@ def extract_table():
     r = requests.get('http://www.annualreports.com/Companies?search=')
     soup = bs(r.content, 'lxml')
     df = pd.DataFrame([(i.text, 'http://www.annualreports.com' + i['href']) for i in soup.select('tbody td:nth-of-type(1) a')], columns = ['Company','Link'])
-    df.to_pickle('links.pkl')
+    df.to_pickle('links.pkl') # saves into a dataframe the name of the company plus the href link it points to
 
 def scrap_pdfs():
     df = pd.read_pickle('links.pkl')
+    a = 0 # for naming the filenames
     
     for x in range(df.Link.count()): 
                 
-        url = df['Link'][x]
+        url = df['Link'][x] # reads the "Link" column from the dataframe
 
-        #If there is no such folder, the script will create one automatically
-        folder_location = r'/home/duke/Annual_Reports/Data' #SPECIFY FULL DIRECTORY
+        folder_location = r'/home/duke/Annual_Reports/Data' #SPECIFY FULL DIRECTORY (if folder doesn't exist will be created automatically)
 
         response = requests.get(url)
         soup= bs(response.text, "html.parser")     
-        for link in soup.select("a[href$='.pdf']"):
-            #Name the pdf files using the last portion of each link which are unique in this case
-            filename = os.path.join(folder_location,link['href'].split('/')[-1])
+
+        for link in soup.select("a[href$='.pdf']"): #go through all the pdfs in all the href links
+            #filename = os.path.join(folder_location,link['href'].split('/')[-1]) # Name the pdf files using the last portion of each link
+            
+            filename = os.path.join(folder_location, str(a))
+            a+=1 # file names are worthless for my task and naming them numerically would be more elegant
+
             with open(filename, 'wb') as f:
                 f.write(requests.get(urljoin(url,link['href'])).content)
 
